aa444b6b36287a5de20e21a1e42a7865b0eef3e8
Marica Björklund <marica.bjorklund@hotmail.com>
2022-11-21 11:39:37 +0100

    Grund2

--- src/Server.java
+++ src/Server.java
@@ -4,12 +4,10 @@ import java.rmi.ServerError;
 
 public class Server {
 
-
     public Server()  {
 
+        try(ServerSocket socket = new ServerSocket(41994)) {
 
-
-        try(ServerSocket socket = new ServerSocket(10667)) {
             while (true) {
 
                 GameRoom game = new GameRoom();
@@ -19,19 +17,20 @@ public class Server {
                 ServerPlayer player2 = new ServerPlayer(socket.accept(),game);
 
                 player1.start();
+
                 player2.start();
 
+                player1.setOpponent(player2);
+
+                player2.setOpponent(player1);
 
             }
-
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-
     }
-
 
     public static void main(String[] args) {
         Server s = new Server();
