e1561c2bd61bbd36db85b346964bbff21761f83e
Marcel <mghostine@icloud.com>
2022-11-22 11:01:42 +0100

    latest version

--- src/Category.java
+++ src/Category.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.util.Properties;
 
 public class Category extends JFrame {
 
@@ -17,6 +19,28 @@ public class Category extends JFrame {
         setVisible(true);
         setSize(450,500);
         setLocationRelativeTo(null);
+
+
+        Properties p = new Properties();
+        try{
+            p.load(new FileInputStream("src/Settings.properties"));
+        }catch (Exception e){
+            System.out.println("Filen kunde inte hittas");
+        }
+        category1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                /*hämta txt filen
+                nya jbuttons med svar
+                ny actionevent
+                if-sats true/false visar rätt svar
+                */
+                int rounds;
+                int questions;
+                rounds = Integer.parseInt(p.getProperty("rounds"));
+                questions = Integer.parseInt(p.getProperty("questions"));
+            }
+        });
     }
 
 
