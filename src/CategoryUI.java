804d0e27d6606b7a0a2f1579ce64556d15caaa41
Marcel <mghostine@icloud.com>
2022-11-22 11:36:49 +0100

    latest version

--- src/CategoryUI.java
+++ src/CategoryUI.java
@@ -1,4 +1,10 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.util.Properties;
 
 public class CategoryUI extends JFrame {
 
@@ -14,6 +20,26 @@ public class CategoryUI extends JFrame {
         setVisible(true);
         setSize(450,500);
         setLocationRelativeTo(null);
+
+        Properties p = new Properties();
+
+        try{
+            p.load(new FileInputStream("src/Settings.properties"));
+        }catch(Exception e){
+            System.out.println("Filen kunde inte hittas");
+        }
+        category1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int question = Integer.parseInt(p.getProperty("questions"));
+                int rounds = Integer.parseInt(p.getProperty("rounds"));
+                /*
+                hämta txt frågor
+                nya jbutton knappar
+                true/false sats
+                 */
+            }
+        });
     }
 
 
